34dee360ff4d2614b01636f68a34cee01cbda30e
symgch <98917895+symgch@users.noreply.github.com>
2022-11-24 16:27:44 -0500

    Taking out unnecessary code

--- assign2/src/components/Card.js
+++ assign2/src/components/Card.js
@@ -17,29 +17,3 @@ export default function Card({ card, handleChoice, flipped }) {
   )
 }
 
-
-// class Card extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {isFlipped: false}
-//   }
-
-//   handleClick = () => {
-//     this.setState(state => ({isFlipped: !state.isFlipped}));
-//   }
-  
-//   render() {
-//     if(this.state.isFlipped === false) {
-//       return (
-//         <div style={{height:'100px', width:'75px', backgroundColor:'black', borderStyle:'solid', borderColor:'red'}}
-//              onClick={this.handleClick}></div>
-//       );
-// 		}
-//     return (
-//       <div style={{height:'100px', width:'75px', backgroundColor:'lightblue', borderStyle:'solid', borderColor:'red'}}
-//            onClick={this.handleClick}>{this.props.cardText}</div>
-//     );
-//   }
-// }
-
-// export default Card;
